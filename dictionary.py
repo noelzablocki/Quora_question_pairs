651512a102c67581865c47ed541d7d235056c674
leon <37694395+zbck@users.noreply.github.com>
2018-08-16 11:09:41 +0200

    Update comments

--- dictionary.py
+++ dictionary.py
@@ -2,6 +2,8 @@
 import numpy as np
 
 def makeDictionary():
+	''' Make a dictionnary. Each word is associated with a vector
+	'''
 	# Load all the vect
 	embedding = np.load("models/noel_embeddings.npy")
 	# print(embedding.shape)
@@ -10,11 +12,6 @@ def makeDictionary():
 	with open("models/wiki_vocab.txt","r") as f:
 		vocab = f.read().splitlines()
 
-	# print(len(vocab))
-	# print(vocab[0])
-
 	# Create the dictionary
 	embedding_dic = {word: embedding[i] for i, word in enumerate(vocab)}
-
-	# embedding_dic["the"]
 	return embedding_dic
